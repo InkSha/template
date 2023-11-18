9a1009e44cac7c1e8fb63007507f8202d9b43598
InkSha <x1a010@163.com>
2023-11-18 15:12:07 +0800

    feat: add type field in package.json so change config file format from js to cjs.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-/**
- * 字符串例子
- * @param arg 任意的字符串参数
- * @returns 传入的字符串参数或默认的 a
- */
-export function exampleString(arg?: string): string {
-  return arg ?? 'a'
+import { ExampleType } from './types'
+
+export const exampleString: ExampleType = (arg) => {
+  return arg || 'a'
 }
+
+console.log(exampleString())
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+/**
+ * 示例类型
+ * @param arg 可选的字符串参数
+ * @returns 处理后的字符串参数
+ */
+export type ExampleType = (arg?: string) => string
