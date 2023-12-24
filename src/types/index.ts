786fd5d7aa340a08bddd15bb29219b706ec0e35e
InkSha <x1a010@163.com>
2023-12-24 16:34:07 +0800

    feat: defined some image and file types.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,48 @@
-/**
- * 示例类型
- * @param arg 可选的字符串参数
- * @returns 处理后的字符串参数
- */
-export type ExampleType = (arg?: string) => string
+export interface ImageOptions {
+  quality?: number
+  fields?: string
+  upload?: string
+  see?: string
+  storage?: string
+  dest?: string
+  width?: number
+  height?: number
+  rename?: (rename: string, old: string) => string
+}
+
+export enum users {
+  root,
+  admin,
+  system,
+  dba,
+  log,
+  test,
+  users,
+  guest,
+  everyone,
+  prohibit,
+  ban,
+}
+
+export enum operateCode {
+  create,
+  rename,
+  read,
+  edit,
+  copy,
+  move,
+  remove,
+  recycle,
+}
+
+export interface operateInfo<User = users> {
+  operator: User
+  code: operateCode
+  beforePath: string[]
+  afterPath: string[]
+  date: string
+  time: string
+  raw: string[]
+  edit: string[]
+  tmp?: Date
+}
