57b4a91c75d3152500bd8439fb3879009ea7d476
InkSha <x1a010@163.com>
2024-03-07 11:15:17 +0800

    feat: internationalization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,71 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import Image from 'next/image'
+import { AllLanguageKeys, AllLanguage, ResourcesNamespace } from '@/i18n'
+import styles from './page.module.css'
+import { useEffect, useState } from 'react'
+import { useTranslation, Trans } from 'react-i18next'
+
+// 按需加载语言文件
+import zh from '@/i18n/zh/home.json'
+import en from '@/i18n/en/home.json'
 
 export default function Home() {
+  fetch('https://v.api.aa1.cn/api/yiyan/index.php', { method: 'get' }).then(
+    (res) => {
+      console.log(res)
+    }
+  )
+
+  const [languageOptions, updateLanguageOptions] = useState<AllLanguage[]>([])
+  const [languageMode, updateLanguageMode] = useState<AllLanguage>(
+    AllLanguage.en
+  )
+  const { t, i18n } = useTranslation()
+
+  const toggleLanguage = (mode: AllLanguage = languageMode) => {
+    i18n.changeLanguage(mode)
+    i18n.addResourceBundle(mode, ResourcesNamespace, mode === 'en' ? en : zh)
+  }
+
+  useEffect(() => {
+    updateLanguageOptions(AllLanguageKeys)
+    toggleLanguage(AllLanguageKeys[0])
+  }, [])
+
+  useEffect(() => {
+    toggleLanguage()
+  }, [languageMode])
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          {t('getStared')}
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
+        <select
+          className={styles.selection}
+          onChange={(e) => {
+            updateLanguageMode(e.target.value.trim() as AllLanguage)
+          }}
+        >
+          {languageOptions.map((v) => (
+            <option key={v} value={v}>
+              {v}
+            </option>
+          ))}
+        </select>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            By{" "}
+            By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src='/vercel.svg'
+              alt='Vercel Logo'
               className={styles.vercelLogo}
               width={100}
               height={24}
@@ -31,65 +78,81 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
+          src='/next.svg'
+          alt='Next.js Logo'
           width={180}
           height={37}
           priority
         />
+        <a
+          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          className={styles.card}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <h2>
+            <span className={styles.to}>
+              <Trans>to</Trans>
+            </span>
+            <span className={styles.link}>
+              <Trans>link</Trans>
+            </span>
+            <span className={styles.see}>
+              <Trans>see</Trans>
+            </span>
+          </h2>
+        </a>
       </div>
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
           className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <h2>
-            Docs <span>-&gt;</span>
+            {t('docs')} <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>{t('docsText')}</p>
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
           className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <h2>
-            Learn <span>-&gt;</span>
+            {t('learn')} <span>-&gt;</span>
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p>{t('learnText')}</p>
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
           className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <h2>
-            Templates <span>-&gt;</span>
+            {t('templates')} <span>-&gt;</span>
           </h2>
-          <p>Explore starter templates for Next.js.</p>
+          <p>{t('templatesText')}</p>
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
           className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <h2>
-            Deploy <span>-&gt;</span>
+            {t('deploy')} <span>-&gt;</span>
           </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <p>{t('deployText')}</p>
         </a>
       </div>
     </main>
-  );
+  )
 }
