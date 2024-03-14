653c06e1a3c55d4c3d4cf6e5c7e632b655a5e1e2
InkSha <x1a010@163.com>
2024-03-14 11:02:38 +0800

    release: remove other, only reserve react template

--- next/src/store/provider.tsx
+++
@@ -1,9 +0,0 @@
-import { store } from '@/store'
-import React, { ReactNode } from 'react'
-import { Provider } from 'react-redux'
-
-const StoreProvider: React.FC<{
-  children: ReactNode
-}> = ({ children }) => <Provider store={store}>{children}</Provider>
-
-export default StoreProvider
--- uni-ts/src/types/index.ts
+++
@@ -1,3 +0,0 @@
-export interface info {
-  id: string
-}
