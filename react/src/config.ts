f87703d5894283cc4a0b99e5325c4d4340ffed60
InkSha <x1a010@163.com>
2024-03-14 10:59:14 +0800

    release: remove other, only reserve nextjs template

--- react/src/config.ts
+++
@@ -1,6 +0,0 @@
-/** global config */
-export const config = {
-  session_key: 'session_key',
-  init_session_key: 'init_session_key',
-  api_domain: '/api/',
-}
--- uni-ts/src/config/net.ts
+++
@@ -1,8 +0,0 @@
-import type { CreateAxiosDefaults } from 'axios'
-
-const net: CreateAxiosDefaults = {
-  baseURL: 'http://localhost:5555',
-  timeout: 3000,
-}
-
-export default net
